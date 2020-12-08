43bcad431289d8d83db4cecda125d5aa9a31b619
Krzysztof <249944@student.pwr.edu.pl>
2020-12-08 13:58:18 +0100

    After meeting

--- src/edu/ib/Main.java
+++ src/edu/ib/Main.java
@@ -20,7 +20,7 @@ public class Main {
             }
         }
 
-        table.getCards().add(deck.getDeck().get(deck.getDeck().size() - 1));
+        table.getCards().add(deck.getDeck().get(deck.getDeck().size() - 1)); // karta na stół
         deck.getDeck().remove(deck.getDeck().size() - 1);
         Scanner scan1 = new Scanner(System.in);
 
@@ -29,38 +29,43 @@ public class Main {
         Boolean winner = true;
         String winnerName = "";
 
-        while (winner) {
+        while (winner) { // rozgrywka
 
             for (int i = 0; i < p.length; i++) { // tura gry
                 Boolean flag1 = true;
                 System.out.println("GRACZ " + (i + 1));
 
                 while (flag1) {
+
                     if (deck.getDeck().size() == 0) {  // sprawdzenie czy deck jest pusty
                         Boolean continueplay = false;
                         for (int j = 0; j < p[i].getHand().size(); j++) {
                             if (p[i].getHand().get(j).getSuit().equals(table.getCards().get(table.getCards().size() - 1).getSuit())
-                                    || p[i].getHand().get(j).getRank().equals(table.getCards().get(table.getCards().size() - 1).getRank())) {
+                                    || p[i].getHand().get(j).getRank().equals(table.getCards().get(table.getCards().size() - 1).getRank())
+                                    || p[i].getHand().get(j).getRank().equals("8")) {
                                 continueplay = true;
                             }
                         }
-                        ArrayList<Card> newDeck = new ArrayList<>(); //przetasowanie
+
+                        ArrayList<Card> newDeck = new ArrayList<>(); //przetasowanie nowego stosu
                         for (int j = 0; j < table.getCards().size() - 1; j++) {
                             newDeck.add(table.getCards().get(j));
                             table.getCards().remove(j);
-                            deck.setDeck(newDeck);
                         }
-                        if (!continueplay) { //sprawdzenie czy gre można kontynuować
-                            System.out.println("Brak możliwości, koniec tury");
+                        deck.setDeck(newDeck);
+                        deck.shuffle();
+                        if (!continueplay) { //sprawdzenie czy turę można kontynuować
+                            System.out.println("Brak kart na stosie, koniec tury");
                             break;
                         }
                     }
+
                     System.out.println("Twoje karty:");
                     for (int j = 0; j < p[i].getHand().size(); j++) {
                         System.out.println(p[i].getHand().get(j).getRank() + p[i].getHand().get(j).getSuit());
                     }
 
-                    System.out.println("Karta na stole:");
+                    System.out.println("\nKarta na stole:");
                     System.out.println(table.getCards().get(table.getCards().size() - 1).getRank()
                             + table.getCards().get(table.getCards().size() - 1).getSuit());
 
@@ -69,8 +74,7 @@ public class Main {
                         rank = scan1.nextLine();
                         suit = scan1.nextLine();
 
-                        if (rank.equals("Pociągnij")) { // gracz dobiera kartę
-                            // powinno być sprawdzenie czy deck nie jest pusty
+                        if (rank.equals("Pociągnij")) { // gracz dobiera karty
                             p[i].getHand().add(deck.getDeck().get(deck.getDeck().size() - 1));
                             deck.getDeck().remove(deck.getDeck().size() - 1);
                             break;
@@ -106,7 +110,7 @@ public class Main {
                     }
                 }
 
-                if (p[i].getHand().size() == 0) { // wygrana
+                if (p[i].getN() == 0) { // wygrana
                     winner = false;
                     winnerName = "Player" + (i + 1);
                     System.out.println("Winner: " + winnerName);
