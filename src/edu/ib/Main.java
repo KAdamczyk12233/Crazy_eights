ff5b461473f3bd83a2634de5db260a0b4d2ffe9c
trywia <75078626+trywia@users.noreply.github.com>
2020-12-08 12:10:33 +0100

    Update Main.java
    Nie zmieniłam tego z ciągnięciem karty, ale trzeba tam będzie jakoś dodać tę opcję

--- src/edu/ib/Main.java
+++ src/edu/ib/Main.java
@@ -18,58 +18,76 @@ public class Main {
             p[1].getHand().add(deck.getDeck().get(deck.getDeck().size() - 1));
             deck.getDeck().remove(deck.getDeck().size() - 1);
         }
+        
         table.getCards().add(deck.getDeck().get(deck.getDeck().size() - 1));
         deck.getDeck().remove(deck.getDeck().size() - 1);
         Scanner scan1 = new Scanner(System.in);
+        
         String rank = "";
         String suit = "";
         Boolean winner = true;
         String winnerName = "";
+        
         while (winner) {
-            for (int i = 0; i < p.length; i++) {
+            
+            for (int i = 0; i < p.length; i++) { // tura gry
                 Boolean flag1 = true;
                 System.out.println("GRACZ " + (i + 1));
+                
                 while (flag1) {
                     System.out.println("Twoje karty:");
                     for (int j = 0; j < p[i].getHand().size(); j++) {
                         System.out.println(p[i].getHand().get(j).getRank() + p[i].getHand().get(j).getSuit());
                     }
+                    
                     System.out.println("Karta na stole:");
-                    System.out.println(table.getCards().get(table.getCards().size() - 1).getRank() + table.getCards().get(table.getCards().size() - 1).getSuit());
-                    if (deck.getDeck().size() == 0) break;
-                    while (flag1) {
+                    System.out.println(table.getCards().get(table.getCards().size() - 1).getRank() 
+                                       + table.getCards().get(table.getCards().size() - 1).getSuit());
+                    
+                    if (deck.getDeck().size() == 0) break; // sprawdzenie czy deck jest pusty
+                    
+                    while (flag1) { // ruch gracza
                         rank = scan1.nextLine();
                         suit = scan1.nextLine();
-                        if (rank.equals("Pociągnij")) {
+                        
+                        if (rank.equals("Pociągnij")) { // gracz dobiera kartę
+                            // powinno być sprawdzenie czy deck nie jest pusty
                             p[i].getHand().add(deck.getDeck().get(deck.getDeck().size() - 1));
                             deck.getDeck().remove(deck.getDeck().size() - 1);
                             break;
-                        }
+                         }
+                        
                         Boolean match = true;
                         Boolean hasCard = false;
+                        
                         for (int j = 0; j < p[i].getHand().size(); j++) {
+                            
                             if (p[i].getHand().get(j).getRank().equals(rank) && p[i].getHand().get(j).getSuit().equals(suit)) {
                                 hasCard = true;
                                 match = false;
+                                
                                 if (p[i].getHand().get(j).getSuit().equals(table.getCards().get(table.getCards().size() - 1).getSuit())
                                         || p[i].getHand().get(j).getRank().equals(table.getCards().get(table.getCards().size() - 1).getRank())
                                         || p[i].getHand().get(j).getRank().equals("8")) {
-                                    match = true;
-                                    table.getCards().add(p[i].getHand().get(j));
-                                    p[i].getHand().remove(j);
+                                    match = true; // karta pasuje
+                                    table.getCards().add(p[i].getHand().get(j)); // dodanie karty do stołu
+                                    p[i].getHand().remove(j); // usunięcie karty od gracza
                                     flag1 = false;
                                 }
                             }
                         }
-                        if (!hasCard) {
+                        
+                        if (!hasCard) { // gracz nie ma karty, którą zadeklarował
                             System.out.println("Nie posiadasz tej karty");
                         }
-                        if (!match) {
+                        
+                        if (!match) { // karta, którą zadeklarował gracz, nie pasuje
                             System.out.println("Karta nie pasuje");
                         }
                     }
                 }
-                if (p[i].getHand().size() == 0) {
+                
+                if (p[i].getHand().size() == 0) { // wygrana
                     winner = false;
                     winnerName = "Player" + (i + 1);
                     System.out.println("Winner: " + winnerName);
